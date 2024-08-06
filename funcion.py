a8ad7a158709f46b834a0322c66a158a5897d6e1
César Antonio <156535675+oigaprof@users.noreply.github.com>
2024-08-06 10:35:02 -0600

    Create funcion.py

---
+++ funcion.py
@@ -0,0 +1,9 @@
+# Un parámetro es una variable definidad por un método que recibe un valor
+# cuando se llama al método.
+
+#Un argumento es un valor que se pasa a un método cuando es invocado
+
+def suma (a, b):
+  return (a + b)
+
+print(suma(10,20))
